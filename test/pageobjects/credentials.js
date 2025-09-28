9c0f51151eda5a5b535c55234995907d29947f0a
dhms013 <dhimas3593@gmail.com>
2025-09-29 03:26:41 +0700

    UPDATE: .gitignore

---
+++ test/pageobjects/credentials.js
@@ -0,0 +1,30 @@
+module.exports = {
+    validUser: {
+      username: 'standard_user',
+      password: 'secret_sauce'
+    },
+    invalidUser: {
+      username: 'standard_user',
+      password: 'wrongpassword'
+    },
+    lockedAccount: {
+      username: 'locked_out_user',
+      password: 'secret_sauce'
+    },
+    problemUser: {
+      username: 'problem_user',
+      password: 'secret_sauce'
+    },
+    performanceGlitchUser: {  
+      username: 'performance_glitch_user',
+      password: 'secret_sauce'
+    },
+    errorUser:{
+      username: 'error_user',
+      password: 'secret_sauce'
+    },
+    visualUser:{
+      username: 'visual_user',
+      password: 'secret_sauce'
+    }
+  };
