ffdada6747a0240070c541d20dd2cd2e64f823df
dhms013 <dhimas3593@gmail.com>
2026-01-30 17:09:05 +0700

    UPDATE: Packages version

--- wdio.conf.js
+++ wdio.conf.js
@@ -55,7 +55,7 @@ exports.config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome',
+        browserName: 'Firefox',
     }],
 
     //
